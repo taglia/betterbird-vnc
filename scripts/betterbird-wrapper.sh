40455341971596426664533660798f959f717843
Cesare <cesare@debian-proxmox>
2025-12-13 04:06:28 +0000

    Fix environment variable passing to betterbird process
    - Add HOME=/home/betterbird to betterbird program environment
    - Add passenv=* to betterbird program section to pass all env vars
    - Remove broken export line from wrapper script
    
    This allows all user-defined environment variables (BETTERBIRD_PROFILE, GDK_BACKEND, MOZ_ENABLE_WAYLAND, etc.) to be passed through to the betterbird process.

--- scripts/betterbird-wrapper.sh
+++ scripts/betterbird-wrapper.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Wrapper script to conditionally launch BetterBird with or without --profile flag
 
-# Export all current environment variables to ensure they're passed to Betterbird
-# This allows users to set any custom environment variables in docker-compose.yml
-export $(env | cut -d= -f1)
-
 if [ -n "$BETTERBIRD_PROFILE" ]; then
     # Use specific profile directory
     exec /opt/betterbird/betterbird --profile "$BETTERBIRD_PROFILE"
